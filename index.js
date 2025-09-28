db2c0ae6fd8a3f83ea83907dcc833ce9fca342e0
Alamgir Khan <Alamgirkhkhqwer@gmail.com>
2025-09-28 14:39:19 +0500

    serverless set up

--- index.js
+++ index.js
@@ -2,27 +2,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const userRoutes = require("./Routes/userRoute.js")
+const userRoutes = require("./Routes/userRoute.js");
+const serverless = require("serverless-http");
 
 dotenv.config();
-
 const app = express();
+
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://e-library-frontend-cyan.vercel.app/'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-  }));
-
+  origin: ['http://localhost:5173', 'https://e-library-frontend-cyan.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-  
 app.use("/api/users", userRoutes);
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
+module.exports.handler = serverless(app);
