5c96a2dfcd66135946db19fdfb7071fd7fe8e91c
ALAMGIRKHAN889 <149321172+ALAMGIRKHAN889@users.noreply.github.com>
2026-02-07 15:12:14 +0500

    Update User.js

--- Models/User.js
+++ Models/User.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
+    role: { type: String, required: true },
     name: { type: String, required: true },
     password: { type: String, required: true }, 
     department: { type: String, required: true },
